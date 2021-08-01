a72892c8fd1cb7c9cf5cca6c42d9f376508101e7
KKambi <bok03220@gmail.com>
2021-08-02 00:21:40 +0900

    feat: request header 출력

--- src/main/java/com/example/userservice/WelcomeController.java
+++ src/main/java/com/example/userservice/WelcomeController.java
@@ -1,6 +1,7 @@
 package com.example.userservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
 
     @GetMapping("/welcome")
-    public String welcome() {
-        return "welcome-user-service";
+    public String welcome(@RequestHeader("user-request") String header) {
+        return "welcome-user-service (header: " + header + ")";
     }
 }
